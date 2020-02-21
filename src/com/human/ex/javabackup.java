20ee182e13f3ad42b70655a4175ea8d26f1a0cec
HU-01 <HU-01@HU-01-PC>
2020-02-21 15:55:42 +0900

    2020.02.21 upload

---
+++ src/com/human/ex/javabackup.java
@@ -0,0 +1,13 @@
+package com.human.ex;
+
+public class javabackup {
+
+	public static void main(String[] args) {
+		//이클립스 프로그램으로 가지고 올려면 Project Explorer에서 파일 하나 선택 후 마우스 오른쪽 클릭 후
+		//Import 선택 후 General 선택 후 Existing Project....를 선택후 진행하면 됨
+		//프로그램 파일을 삭제할 땐 꼭 Delete.....표시되어있는거 체크 후 확인하기
+		//github는 코드 관리하는데 지금은 파일백업으로만 사용하는 것이 안전하다고 생각함
+		//github에서 
+	}
+
+}
