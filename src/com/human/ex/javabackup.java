7a7ef698fe0cf1d3719e85deba4956ba8b13d8eb
HU-01 <HU-01@HU-01-PC>
2020-02-21 16:15:31 +0900

    2020.02.21 업로드

--- src/com/human/ex/javabackup.java
+++ src/com/human/ex/javabackup.java
@@ -7,7 +7,9 @@ public class javabackup {
 		//Import 선택 후 General 선택 후 Existing Project....를 선택후 진행하면 됨
 		//프로그램 파일을 삭제할 땐 꼭 Delete.....표시되어있는거 체크 후 확인하기
 		//github는 코드 관리하는데 지금은 파일백업으로만 사용하는 것이 안전하다고 생각함
-		//github에서 
+		//github에서 백업할려면 github에 로그인 후 마이페이지 링크복사를 한 후 Project Explorer에서 보낼 파일 선택 후 오른쪽
+		// 클릭 후 Team 클릭 후 share Project 클릭 후 다시 Team 들어가서 commit 클릭 후 unstaged 라인에 플러스 두개표시
+		//클릭하여 staged changes로 이동하면 맨아래 메시지에 기입후 
 	}
 
 }
